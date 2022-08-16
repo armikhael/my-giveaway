d5ab7b1b5f2324ae6cfbd16e3cb2040158b2dcc2
Carlos Espinoza <carlosarmikhael@gmail.com>
2022-08-15 21:50:08 -0400

    chore: warning next/core-web-vitals eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,12 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["plugin:react/recommended", "standard", "prettier"],
+  extends: [
+    "plugin:react/recommended",
+    "standard",
+    "prettier",
+    "next/core-web-vitals",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
