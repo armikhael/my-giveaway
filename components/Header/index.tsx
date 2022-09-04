299bfd7a852c05a93f97df980cad93eee238d1e7
Carlos Espinoza <carlosarmikhael@gmail.com>
2022-09-04 17:22:19 -0400

    feat: install font, Logo main

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,8 +1,11 @@
 import styles from "./styles.module.scss";
 
+import Logo from "@assets/Logo";
+
 function Header() {
   return (
     <>
+      <Logo width="100" height="100" />
       <h1 className={styles.title}>Header</h1>
     </>
   );
