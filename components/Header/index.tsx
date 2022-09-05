c3519b194cea7da4dd1ed91e176a2ba8f15de01a
Carlos Espinoza <carlosarmikhael@gmail.com>
2022-09-05 00:30:29 -0400

    feat: new header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,12 +1,37 @@
-import styles from "./styles.module.scss";
+import Link from "next/link";
+
+import main from "@styles/main.module.scss";
+import header from "./header.module.scss";
 
 import Logo from "@assets/Logo";
 
 function Header() {
   return (
     <>
-      <Logo width="100" height="100" />
-      <h1 className={styles.title}>Header</h1>
+      <header className={header.container}>
+        <div className={main.container}>
+          <div className={header.container__content}>
+            <div className={header.section__one}>
+              <Link href="/">
+                <div className={header.logo}>
+                  <Logo width="36" height="36" />
+                  <span className={header.logo__title}>
+                    Mi<span>Sorteo</span>
+                  </span>
+                </div>
+              </Link>
+              <div className={header.options}>
+                <Link href="/">Precios</Link>
+                <Link href="/">Ayuda</Link>
+              </div>
+            </div>
+            <div className={header.auth}>
+              <Link href="/">Inicio Sesión</Link>
+              <Link href="/">Registrarme</Link>
+            </div>
+          </div>
+        </div>
+      </header>
     </>
   );
 }
